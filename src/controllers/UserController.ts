1b4fe5698759ef2a79d5330f69779e8e4ad61a7d
hiagopsilva <hiagohps360@gmail.com>
2020-05-26 00:44:53 -0300

    add emailService

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response} from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
   { name: 'Hiago', email: 'hiago@teste.com' }
@@ -7,5 +8,16 @@ const users = [
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail(
+      { name: 'Hiago Prates', email: 'hiago@teste.com' },
+      { subject: 'Bem vindo ao sistema', body: 'Seja bem-vindo' }
+    );
+
+    return res.send();
   }
  };
